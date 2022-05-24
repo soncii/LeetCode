c890715c90454a9ac9679d143b13ddc7a784ea10
soncii <Damir0110gim@gmail.com>
2022-05-25 00:12:54 +0600

    array101

---
+++ removeDuplicatesfromSortedArray.cpp
@@ -0,0 +1,10 @@
+int removeDuplicates(vector<int>& nums) {
+        int j=0;
+        for (int i=1; i<nums.size(); i++) {
+            if (nums[i]!=nums[j]) {
+                j++;
+                nums[j]=nums[i];
+            }
+        }
+        return j+1;
+    }
